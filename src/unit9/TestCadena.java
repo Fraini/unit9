dec1c21312078509466f3d605e3a58d81570a896
Fraini <jarabaco@gmail.com>
2015-05-11 22:02:05 +0200

    Añadido el caso b y c --> pasan sin problemas

--- src/unit9/TestCadena.java
+++ src/unit9/TestCadena.java
@@ -9,9 +9,34 @@ public class TestCadena {
 
 	@Test
 	public void testSonIguales() {
+		
+		/*
+ 			 a) cadena1: entornos, cadena2: ento. Para conocer el resultado emplearemos el método
+			equals de la clase String del API de Java
+		 */
+		
 		String a = "entornos";
 		String b = "nos";
 		assertEquals(false,new CadenaAlumnos().sonIguales(a, b));	
+		
+		/*
+		 *  b) cadena1: ento, cadena2: entornos. Para conocer el resultado emplearemos el método
+			equals de la clase String del API de Java
+		 */
+		
+		 a = "nos";
+		 b = "entornos";
+		assertEquals(false,new CadenaAlumnos().sonIguales(a, b));	
+		
+		/*
+		 * c) cadena1: entornos, cadena2: sonrotne. Para conocer el resultado emplearemos el método
+			equals de la clase String del API de Java
+		 */
+		
+		 a = "entornos";
+		 b = "sonrotne";
+		assertEquals(false,new CadenaAlumnos().sonIguales(a, b));	
+		
 	}
 
 }
