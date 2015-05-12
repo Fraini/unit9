1501319d6ff5d45ba08c631c5146cd1c11cadcb7
Fraini <jarabaco@gmail.com>
2015-05-12 11:01:12 +0200

    Pasando pruebas a los métodos con Junit, unificando todos en una única clase.

--- src/unit9/CadenaAlumnos.java
+++ src/unit9/CadenaAlumnos.java
@@ -23,16 +23,18 @@ public class CadenaAlumnos {
 	//Método que devuelve si la primera cadena es mayor o no
 	public boolean esMayor (String cadena1, String cadena2)
 	{
-		if(cadena1.length() > cadena2.length())
+		if(cadena1.compareTo(cadena2)>0){
 			return true;
-		else
+		}
+		else{
 			return false;
+		}
 	}
 	
 	//Método que devuelve si la primera cadena es menor o no
 	public boolean esMenor (String cadena1, String cadena2)
 	{
-		if(cadena1.length() < cadena2.length())
+		if(cadena1.compareTo(cadena2)< 0)
 			return true;
 		else
 			return false;
@@ -42,99 +44,46 @@ public class CadenaAlumnos {
 	//hay por cambio
 	public String reemplazaTodos (String cadena, String regla, String cambio)
 	{
-		/*char[] Caracteres = cadena.toCharArray();
-		char[] Regla = regla.toCharArray();
-		char[] Cambio = cambio.toCharArray();
-		String aux ="";
-		int i= 0;	
-		for(int x= 0; x < Caracteres.length; x++)
-		{
-			if(Caracteres[x] == Regla[i] )
-			{
-				aux += Cambio[i];	
-			}
-		}
-		System.out.println("[" + x + "] " + Caracteres[x]);
-		 */
-		return "";
+		String aux = "";
+		aux = cadena.replaceAll(regla, cambio);
+		return aux;
 	}
 			
 	public String reemplazaPrimero(String cadena, String regla, String cambio)
 	{
-		char[] Caracteres = cadena.toCharArray();
-		char[] Regla = regla.toCharArray();
-		char[] Cambio = cambio.toCharArray();
 		String aux = "";
-		for(int i= 0; i < Caracteres.length; i++)
-		{
-			if(Caracteres[i] == Regla[0])
-				aux += Cambio[i];
-			else
-				aux +=Caracteres[i];
-		}
+		aux = cadena.replaceFirst(regla, cambio);
 		
 		return aux;
 	}
 	
 	public String quitaEspacios(String cadena)
 	{
-		char[] caracteres = cadena.toCharArray();
-		String aux = "";
-		
-		for(int i= 0; i < caracteres.length; i++)
-		{
-			if(caracteres[i] != ' ')
-			{
-				aux += caracteres[i];
-			}
-		}
+		String aux = cadena.trim();
 		return aux;
 	}
 	
 	public String convertirMayusculas(String cadena)
 	{
-		char[] caracteres = cadena.toCharArray();
-		String aux = "";
-		
-		for(int i=0; i < caracteres.length; i++)
-		{
-			int letraM = 'A' + (caracteres[i] - 'a');
-			aux += (char) letraM;
-		}
+		String aux = cadena.toUpperCase();
 		return aux;
 	}
 
 	public String convertirMinusculas(String cadena)
 	{
-		char[] caracteres = cadena.toCharArray();
-		String aux = "";
-		
-		for(int i=0; i < caracteres.length; i++)
-		{
-			int letraM = 'a' + (caracteres[i] - 'A');
-			aux += (char) letraM;
-		}
+		String aux = cadena.toLowerCase();
 		return aux;
 	}
 	
 	public long longitudCadena(String cadena)
 	{
-		char[] caracteres = cadena.toCharArray();
-		
-		long contador = 0;
-		
-		for(int i= 0; i < caracteres.length; i++)
-		{
-			contador++;
-		}
+		long contador = cadena.length();
 		return contador;
 	}
 	
 	public boolean empiezaCon(String cadena, String prefijo)
 	{
-		char[] cCadena = cadena.toCharArray();
-		char[] cPrefijo = prefijo.toCharArray();
-		if(cCadena[0] == cPrefijo[0])
+		if(cadena.startsWith(prefijo))
 			return true;
 		else
 			return false;
@@ -142,9 +91,7 @@ public class CadenaAlumnos {
 
 	public boolean acabaEn(String cadena, String sufijo)
 	{
-		char[] cCadena = cadena.toCharArray();
-		char[] cPrefijo = sufijo.toCharArray();
-		if(cCadena[cadena.length() -1] == cPrefijo[0])
+		if(cadena.endsWith(sufijo))
 			return true;
 		else
 			return false;
@@ -153,23 +100,9 @@ public class CadenaAlumnos {
 	
 	public long posicionPrimeraCadena(String cadena, String letra)
 	{
-		long posicion = 0;
-		char[] cCadena = cadena.toCharArray();
-		char[] cLetra = letra.toCharArray();
-		boolean found = false;
-		int i=0;
-		while( i < cCadena.length && !found)
-		{
-			if(cCadena[i] == cLetra[0])
-			{
-				found = true;
-			}
-			posicion++;
-			i++;
-		}
 		
-		if(found)
-			return posicion;
+		if(cadena.indexOf(letra)> -1)
+			return cadena.indexOf(letra);
 		else
 			return -1;
 	}
@@ -209,13 +142,7 @@ public class CadenaAlumnos {
 	//Método compara ignorando mayuscula
 		public boolean comparaIgnorandoMayusculas(String cadena1, String cadena2)
 		{
-			//creo un objeto de la clase cadena para hacer uso
-			//del metodo {"convertirMayusculas"}.
-			CadenaAlumnos i = new CadenaAlumnos();		
-			String auxCadena1 = i.convertirMayusculas(cadena1);
-			String auxCadena2 = i.convertirMayusculas(cadena2);
-			
-			if(auxCadena1.length() > auxCadena2.length())
+			if(cadena1.equalsIgnoreCase(cadena2))
 				return true;
 			else
 				return false;
---
+++ src/unit9/CadenaAlumnosTest.java
@@ -0,0 +1,210 @@
+package unit9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadenaAlumnosTest {
+
+	@Test
+	public void testCadenaAlumnos() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * 	@parama a) cadena1: entornos, cadena2: ento. Para conocer el resultado emplearemos el método
+		equals de la clase String del API de Java
+		@return false
+	 * */
+	@Test
+	public void testSonIgualesA() {
+		
+		String a = "entornos";
+		String b = "nos";
+		assertEquals(false,new CadenaAlumnos().sonIguales(a, b));	
+			
+	}
+	
+	/**
+	 * 
+   		b) cadena1: ento, cadena2: entornos. Para conocer el resultado emplearemos el método
+		equals de la clase String del API de Java 
+	 * */
+	@Test
+	public void testSonIgualesB(){
+		 String a = "nos";
+		 String b = "entornos";
+		 assertEquals(false,new CadenaAlumnos().sonIguales(a, b));
+	}
+	
+	/**
+	 * c) cadena1: entornos, cadena2: sonrotne. Para conocer el resultado emplearemos el método
+		equals de la clase String del API de Java
+	 * */
+	@Test
+	public void testSonIngualesC(){
+		 String a = "entornos";
+		 String b = "sonrotne";
+		assertEquals(false,new CadenaAlumnos().sonIguales(a, b));	
+	}
+	
+	/**
+	 * d)
+		cadena1: entornos, cadena2: entornos. Para conocer el resultado emplearemos el método 
+		equals de la clase String del API de Java
+	 */
+	@Test
+	public void testSonIgualesD() {
+		String a = "entornos";
+		String b = "entornos";
+		assertEquals(true,new CadenaAlumnos().sonIguales(a, b));
+	}
+	
+
+	/**@param Método que devuelve true si la cadena1 es mayor que la cadena2*/
+	@Test
+	public void testEsMayorA() {
+		String a = "manolo";
+		String b = "mano";
+		
+		assertEquals(true,new CadenaAlumnos().esMayor(a, b));
+	}
+	
+	/**@param Método que devuelve true si la cadena1 es menor que la cadena2*/
+	@Test
+	public void testEsMayorB() {
+		String a = "mano";
+		String b = "manolo";
+		
+		assertEquals(false,new CadenaAlumnos().esMayor(a, b));
+	}
+	
+	/**@param Método que devuelve false si las dos cadenas son iguales*/
+	@Test
+	public void testEsMayorC() {
+		String a = "manolo";
+		String b = "manolo";
+		
+		assertEquals(false,new CadenaAlumnos().esMayor(a, b));
+	}
+
+	/** Si la primera cadena es mayor que la segunda devuelve false*/
+	@Test
+	public void testEsMenorA() {
+		String a = "manolo";
+		String b = "mano";
+		assertEquals(false,new CadenaAlumnos().esMenor(a, b));
+	}
+	
+	/** Si la cadena 2 es mayor que la cadena 1 devuelve true*/
+	@Test
+	public void testEsMenorB() {
+		String a = "mano";
+		String b = "manolo";
+		assertEquals(true,new CadenaAlumnos().esMenor(a, b));
+	}
+	
+	/** Si son iguales devuelve false*/
+	@Test
+	public void testEsMenorC() {
+		String a = "manolo";
+		String b = "manolo";
+		assertEquals(false,new CadenaAlumnos().esMenor(a, b));
+	}
+
+	@Test
+	public void testReemplazaTodos() {
+		String a = "pera";
+		String cambio = "ez";
+		String regla = "a";
+		assertEquals("perez",new CadenaAlumnos().reemplazaTodos(a, regla, cambio));
+	}
+
+	@Test
+	public void testReemplazaPrimero() {
+		String a = "beso";
+		String regla = "b";
+		String cambio = "p";
+		assertEquals("peso",new CadenaAlumnos().reemplazaPrimero(a, regla, cambio));
+	}
+
+	@Test
+	public void testQuitaEspacios() {
+		String a = "                 beso                ";
+		assertEquals("beso",new CadenaAlumnos().quitaEspacios(a));
+	}
+
+	@Test
+	public void testConvertirMayusculas() {
+		String a = "fraini";
+		assertEquals("FRAINI",new CadenaAlumnos().convertirMayusculas(a));
+	}
+
+	@Test
+	public void testConvertirMinusculas() {
+		String a = "FRAINI";
+		assertEquals("fraini",new CadenaAlumnos().convertirMinusculas(a));
+	}
+
+	@Test
+	public void testLongitudCadena() {
+		String a = "FRAINI";
+		assertEquals(6,new CadenaAlumnos().longitudCadena(a));
+	}
+
+	@Test
+	public void testEmpiezaCon() {
+		String a = "FRAINI";
+		String prefijo = "I";
+		assertEquals(true,new CadenaAlumnos().empiezaCon(a, prefijo));
+	}
+
+	@Test
+	public void testAcabaEn() {
+		String a = "FRAINI";
+		String prefijo = "F";
+		assertEquals(true,new CadenaAlumnos().acabaEn(a, prefijo));
+	}
+
+	@Test
+	public void testPosicionPrimeraCadena() {
+		String a = "FRAINI";
+		String letra = "F";
+		assertEquals(0,new CadenaAlumnos().posicionPrimeraCadena(a, letra));
+	}
+
+	@Test
+	public void testExtraerSubstringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExtraerSubstringStringIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConcatenaCadenas() {
+		fail("Not yet implemented");
+	}
+
+	//Método que prueba si las dos cadenas son iguales ignorando mayúsculas
+		@Test
+		public void testIgnorandoMayusculas() {
+			String a = "mAnOlo";
+			String b = "manolo";
+			
+			assertEquals(true,new CadenaAlumnos().comparaIgnorandoMayusculas(a, b));
+		}
+		
+		//Método que prueba si las dos cadenas son iguales ignorando mayúsculas
+		@Test
+		public void testBIgnorandoMayusculas() {
+			String a = "mAnOlo";
+			String b = "MANOLO";
+			
+			assertEquals(true,new CadenaAlumnos().comparaIgnorandoMayusculas(a, b));
+		}
+
+
+}
